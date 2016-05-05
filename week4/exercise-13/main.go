f44baa6e335ac83064d06623c708da6501ae6be7
GeorgeChen <bamoo456@gmail.com>
2016-05-05 08:48:14 +0800

    apply go concurrency into exercise-13

--- week4/exercise-13/main.go
+++ week4/exercise-13/main.go
@@ -15,10 +15,20 @@ func main() {
 	fmt.Println("setup the search target array size and inside elements")
 	targetArray := getFormattedInputs()
 	resultArray := make([]int, len(targetArray), len(targetArray))
+	resultChannels := make([]chan int, len(targetArray), len(targetArray))
 	for i := 0; i < len(resultArray); i++ {
-		resultArray[i] = libs.BinarySearch(sortedArray, 0, len(resultArray)-1, targetArray[i])
+		resultChannels[i] = make(chan int)
+		go goBinarySearch(sortedArray, 0, len(resultArray)-1, targetArray[i], resultChannels[i])
+	}
+	for i := 0; i < len(resultArray); i++ {
+		resultArray[i] = <-resultChannels[i]
 	}
 	fmt.Println("The Search Result is ", resultArray)
+}
+
+func goBinarySearch(array []int, low int, high int, key int, channel chan int) {
+	channel <- libs.BinarySearch(array, low, high, key)
+	close(channel)
 }
 
 func getInput() []string {
